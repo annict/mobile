d8066ae8074a24c587f0beb1381b37c2e85eac28
Koji Shimba <me@shimba.co>
2017-09-19 23:15:57 +0900

    Add react-native-vector-icons

--- src/navigators/MainTabNavigator.js
+++ src/navigators/MainTabNavigator.js
@@ -1,4 +1,6 @@
+import React from "react"
 import { TabNavigator } from "react-navigation"
+import Icon from "react-native-vector-icons/FontAwesome"
 
 import { HomeGuestStackNavigator, SeasonalWorksStackNavigator } from "./"
 
@@ -7,6 +9,7 @@ export const MainTabNavigator = TabNavigator({
     screen: HomeGuestStackNavigator,
     navigationOptions: {
       tabBarLabel: "Home",
+      tabBarIcon: <Icon color="#900" name="home" size={30} />,
     },
   },
 
